cdb84eaee7da7cdeed0e78720c9b07adff4d31d0
Tom Harley <tom@magnostherobot.co.uk>
2023-06-08 01:11:18 +0100

    Break event handling into individual functions

--- src/lib.rs
+++ src/lib.rs
@@ -175,6 +175,58 @@ fn init_logging() {
     }
 }
 
+fn handle_window_event(state: &mut State, event: &WindowEvent) -> Option<ControlFlow> {
+    match event {
+        WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
+            input: KeyboardInput {
+                state: ElementState::Pressed,
+                virtual_keycode: Some(VirtualKeyCode::Escape),
+                ..
+            },
+            ..
+        } => Some(ControlFlow::Exit),
+
+        WindowEvent::Resized(physical_size) => {
+            state.resize(*physical_size);
+            None
+        }
+
+        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+            state.resize(**new_inner_size);
+            None
+        }
+
+        _ => None
+    }
+}
+
+fn handle_redraw_event(state: &mut State) -> Option<ControlFlow> {
+    state.update();
+    match state.render() {
+        Ok(_) => None,
+        Err(wgpu::SurfaceError::Lost) => { state.resize(state.size); None },
+        Err(wgpu::SurfaceError::OutOfMemory) => Some(ControlFlow::Exit),
+        Err(e) => { eprintln!("{:?}", e); None },
+    }
+}
+
+fn handle_event(state: &mut State, event: &Event<()>) -> Option<ControlFlow> {
+    match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if *window_id == state.window().id() && !state.input(event) => {
+            handle_window_event(state, event)
+        }
+
+        Event::RedrawRequested(window_id) if *window_id == state.window().id() => {
+            handle_redraw_event(state)
+        }
+
+        _ => None
+    }
+}
+
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub async fn run() -> Result<(), Box<dyn Error>> {
     init_logging();
@@ -203,47 +255,9 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
             .expect("Couldn't append canvas to document body.");
     }
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == state.window().id() => if !state.input(event) {
-            match event {
-                WindowEvent::CloseRequested
-                | WindowEvent::KeyboardInput {
-                    input:
-                        KeyboardInput {
-                            state: ElementState::Pressed,
-                            virtual_keycode: Some(VirtualKeyCode::Escape),
-                            ..
-                        },
-                    ..
-                } => *control_flow = ControlFlow::Exit,
-
-                WindowEvent::Resized(physical_size) => {
-                    state.resize(*physical_size);
-                }
-
-                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                    state.resize(**new_inner_size);
-                }
-
-                _ => {}
-            }
+    event_loop.run(move |event, _, control_flow| {
+        if let Some(new_flow) = handle_event(&mut state, &event) {
+            *control_flow = new_flow;
         }
-
-        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-            state.update();
-            match state.render() {
-                Ok(_) => {}
-                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
-                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                Err(e) => eprintln!("{:?}", e),
-            }
-        }
-
-        _ => {}
     });
 }
-
-
